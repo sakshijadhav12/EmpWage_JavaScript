1974a28f0992a7186511ddce2cd6a1ffabb21fde
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-08-28 20:56:17 +0530

    employee wage using java script

---
+++ Emp.js
@@ -0,0 +1,17 @@
+const Is_Present = 1;
+let empInput = Math.floor((Math.random() * 10)%2);
+console.log("random input is "+ empInput);
+if(Is_Present==empInput)
+{
+    console.log("Employee is present");
+}
+else
+{
+  console.log("Employee is absent");
+}
+
+   
+   
+    
+
+
