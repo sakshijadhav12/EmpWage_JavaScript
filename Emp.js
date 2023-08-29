0335f2f3ff48c07a51d8de82c6643b62e4839aef
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-08-29 21:07:39 +0530

    [sakshi] calculating wage for month using for loop

--- Emp.js
+++ Emp.js
@@ -1,24 +1,28 @@
-const Is_FullTime = 1;
-const Is_PartTime = 2;
-const WagePerHr = 20;
-const PartTimeHrs = 4;
-const FullTimeHrs = 8;
-let empwage = 0;
-
-function getWorkingHrs() {
-  let empInput = Math.floor((Math.random() * 10) % 3);
-  console.log("random input is " + empInput);
-
-  switch (empInput) {
-    case Is_FullTime: 
-      return FullTimeHrs;
-    case Is_PartTime: 
-      return PartTimeHrs;
-    default: 
+const NUM_OF_WORKING_DAYS = 20;
+const WAGE_PER_HOUR = 20;
+const PART_TIME_HRS = 4;
+const FULL_TIME_HRS = 8;
+let totalEmpHrs = 0;
+let totalEmpWage = 0;
+function getWorkingHours() {
+  let empCheck = Math.floor(Math.random() * 3); // Generates values 0, 1, or 2
+  switch (empCheck) {
+    case 1:
+      return PART_TIME_HRS;
+    case 2:
+      return FULL_TIME_HRS;
+    default:
       return 0;
   }
 }
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+  let empHrs = getWorkingHours();
+  totalEmpHrs += empHrs;
 
-let empHrs = getWorkingHrs(); 
-empwage = empHrs * WagePerHr;
-console.log("daily emp wage is: " + empwage);
+  let empWage = empHrs * WAGE_PER_HOUR;
+  totalEmpWage += empWage;
+
+  console.log("Day " + (day + 1) + " - Total Hrs: " + empHrs + " Emp Wage: " + empWage);
+}
+
+console.log("Total Emp Wage for the month: " + totalEmpWage);
