230058e7bbbcd920ed44b13626a0a65c99ada823
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-08-29 20:47:03 +0530

    [sakshi] modifying program.cs to calculate daily wage using function

--- Emp.js
+++ Emp.js
@@ -1,30 +1,24 @@
 const Is_FullTime = 1;
-const Is_PartTime =2;
+const Is_PartTime = 2;
 const WagePerHr = 20;
-let empHrs =0;
+const PartTimeHrs = 4;
+const FullTimeHrs = 8;
 let empwage = 0;
-let empInput = Math.floor((Math.random() * 10)%3);
-console.log("random input is "+ empInput);
-if(Is_FullTime==empInput)
-    {  empHrs = 8;
-    console.log("Employee is Fulltime employee  and emphrs are " + empHrs);
-    }
-else if (Is_PartTime==empInput)
-{
-   empHrs = 4;
-    console.log("Employee is Part time employee and emphrs are " + empHrs);
-    
-}
-else
-{
-  console.log("Employee is absent and emphrs are "+empHrs);
+
+function getWorkingHrs() {
+  let empInput = Math.floor((Math.random() * 10) % 3);
+  console.log("random input is " + empInput);
+
+  switch (empInput) {
+    case Is_FullTime: 
+      return FullTimeHrs;
+    case Is_PartTime: 
+      return PartTimeHrs;
+    default: 
+      return 0;
+  }
 }
 
-empwage = empHrs*WagePerHr;
-console.log("Daily Employee wage is "+ empwage);
-
-   
-   
-    
-
-
+let empHrs = getWorkingHrs(); 
+empwage = empHrs * WagePerHr;
+console.log("daily emp wage is: " + empwage);
